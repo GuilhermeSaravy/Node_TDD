88fa4d4717a0bf7cf0d49d464a5003235b1fd4e5
Guilherme Holanda Saravi <guilherme.saravi@pmcg.imti>
2021-04-28 15:31:39 -0400

    Implementando BD e Migrations

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: '127.0.0.1',
+  username: 'postgres',
+  password: '123',
+  database: 'nodeauth',
+  dialect: 'postgres',
+  operatorsAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    urderscoredAll: true
+  }
+}
